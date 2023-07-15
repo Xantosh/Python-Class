42931389abf1f951f43c5174fd9caaba28466d80
Santosh Upreti <69950069+Xantosh@users.noreply.github.com>
2023-07-15 18:16:20 +0545

    new

---
+++ calculator.py
@@ -0,0 +1,50 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        return "Error: Division by zero"
+
+print("Welcome to the Calculator!")
+
+while True:
+    print("Please select an operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == '5':
+        print("Calculator program has ended. Goodbye!")
+        break
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == '1':
+        result = add(num1, num2)
+        print("Result: ", result)
+    elif choice == '2':
+        result = subtract(num1, num2)
+        print("Result: ", result)
+    elif choice == '3':
+        result = multiply(num1, num2)
+        print("Result: ", result)
+    elif choice == '4':
+        result = divide(num1, num2)
+        print("Result: ", result)
+    else:
+        print("Invalid input. Please try again.")
+
+    print()  # Empty line for spacing
